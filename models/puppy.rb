b6a05168cd5662527feca6cf2670dbdff29bb21b
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-09-10 22:04:33 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -1,17 +1,17 @@
 class Puppy 
   attr_accessor :name, :breed, :months_old
-  @@all = []
+  # @@all = []
   
   def initialize(name,breed,months_old)
     @name = name 
     @breed = breed 
     @months_old = months_old 
-    @@all << self
+  #  @@all << self
   end 
   
-  def self.all
-    @@all
-  end 
+ # def self.all
+  #  @@all
+  # end 
   
   
 end 
